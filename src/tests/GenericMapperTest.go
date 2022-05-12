28df2999f3dc0ca9c9e2cfb08fa8ae84a45e2782
Insei <goodmobiledevices@gmail.com>
2022-05-12 19:34:04 +0300

    tests: added generic mapper test and ethernet switch mapper tests
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

---
+++ src/tests/GenericMapperTest.go
@@ -0,0 +1,76 @@
+package tests
+
+import (
+	"fmt"
+	"reflect"
+	"rol/app/interfaces"
+	"rol/app/mappers"
+)
+
+type GenericMapperTest[Dto interface{}, Entity interfaces.IEntityModel] struct{}
+
+func NewGenericMapperToEntity[Dto interface{}, Entity interfaces.IEntityModel]() *GenericMapperTest[Dto, Entity] {
+	return &GenericMapperTest[Dto, Entity]{}
+}
+
+func (gme *GenericMapperTest[Dto, Entity]) MapToEntity(dto Dto, entity *Entity) error {
+	err := mappers.MapDtoToEntity(dto, entity)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (gme *GenericMapperTest[Dto, Entity]) MapToDto(entity Entity, dto *Dto) error {
+	err := mappers.MapEntityToDto(entity, dto)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func compareDtoAndEntity(from interface{}, to interface{}) error {
+	fromReflectFields := reflect.TypeOf(from)
+	fromReflectValues := reflect.ValueOf(from)
+
+	num := fromReflectFields.NumField()
+	for i := 0; i < num; i++ {
+		reflectField := fromReflectFields.Field(i)
+		reflectValue := fromReflectValues.Field(i)
+
+		if reflectValue.Kind() == reflect.Struct && reflectField.Tag != "gorm:\"index\"" {
+			if err := compareDtoAndEntity(reflectValue.Interface(), to); err != nil {
+				return err
+			}
+		} else {
+			if !reflectValue.IsValid() {
+				continue
+			}
+
+			var fromFieldValue any
+			if reflectValue.Kind() == reflect.Int {
+				fromFieldValue = int(reflectValue.Int())
+			} else {
+				fromFieldValue = reflectValue.Interface()
+			}
+
+			toReflectValues := reflect.ValueOf(to)
+			toValue := reflect.Indirect(toReflectValues).FieldByName(reflectField.Name)
+			if !toValue.IsValid() {
+				continue
+			}
+
+			var toFieldValue any
+			if toValue.Kind() == reflect.Int {
+				toFieldValue = int(toValue.Int())
+			} else {
+				toFieldValue = toValue.Interface()
+			}
+
+			if !reflect.DeepEqual(fromFieldValue, toFieldValue) {
+				return fmt.Errorf("%s field not the same", reflectField.Name)
+			}
+		}
+	}
+	return nil
+}

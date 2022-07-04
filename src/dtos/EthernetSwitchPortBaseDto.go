cad5268f62c6776f21050ebd0bfff02a97732222
Insei <goodmobiledevices@gmail.com>
2022-07-05 00:02:38 +0300

    src: dtos: ethernet-switch-port: remove switch id field * Switch ID will be passed as a parameter
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

--- src/dtos/EthernetSwitchPortBaseDto.go
+++ src/dtos/EthernetSwitchPortBaseDto.go
@@ -1,11 +1,7 @@
 package dtos
 
-import "github.com/google/uuid"
-
 //EthernetSwitchPortBaseDto base dto for ethernet switch port
 type EthernetSwitchPortBaseDto struct {
-	//EthernetSwitchID - switch id
-	EthernetSwitchID uuid.UUID
 	//POEType type of PoE for this port
 	//can be: "poe", "poe+", "passive24", "none"
 	POEType string

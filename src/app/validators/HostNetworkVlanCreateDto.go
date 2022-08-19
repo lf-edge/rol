71b1b088bf636496e2c566434cb872dcb02fe073
Insei <goodmobiledevices@gmail.com>
2022-08-19 18:57:31 +0300

    src: app: validators: add host network VLAN dto validators
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

---
+++ src/app/validators/HostNetworkVlanCreateDto.go
@@ -0,0 +1,27 @@
+package validators
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation"
+	"rol/dtos"
+)
+
+//ValidateHostNetworkVlanCreateDto validates host network create dto
+//	Return
+//	error - if an error occurs, otherwise nil
+func ValidateHostNetworkVlanCreateDto(dto dtos.HostNetworkVlanCreateDto) error {
+	err := validation.ValidateStruct(&dto,
+		validation.Field(&dto.VlanID, []validation.Rule{
+			validation.Required,
+			validation.Max(4098),
+			validation.Min(1),
+		}...),
+		validation.Field(&dto.Master, []validation.Rule{
+			validation.Required,
+			validation.By(trimValidation),
+			validation.By(containsSpacesValidation),
+		}...),
+		validation.Field(&dto.Addresses, []validation.Rule{
+			validation.By(sliceOfCidrStringsValidation),
+		}...))
+	return convertOzzoErrorToValidationError(err)
+}

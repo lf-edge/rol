f465d50c388cc5d736d66d8eb9e0fcfb9909affa
Insei <goodmobiledevices@gmail.com>
2022-08-19 19:00:17 +0300

    src: app: services: add host network vlan service
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

---
+++ src/app/utils/GenericSlice.go
@@ -0,0 +1,43 @@
+package utils
+
+//SliceContainsElement check what slice contains value
+func SliceContainsElement[T comparable](s []T, elem T) bool {
+	for _, a := range s {
+		if a == elem {
+			return true
+		}
+	}
+	return false
+}
+
+//SliceDiffElements determine slice diff from origin to modified
+//
+//Parameters:
+//	original - original slice
+//	modified - modified slice
+//Returns:
+//	T[] - deleted elements
+//	T[] - added elements
+func SliceDiffElements[T comparable](original []T, modified []T) ([]T, []T) {
+	deletedElems := []T{}
+deleted:
+	for _, elemFromOriginal := range original {
+		for _, elemFromModified := range modified {
+			if elemFromOriginal == elemFromModified {
+				continue deleted
+			}
+		}
+		deletedElems = append(deletedElems, elemFromOriginal)
+	}
+	addedElems := []T{}
+added:
+	for _, elemFromModified := range modified {
+		for _, elemFromOriginal := range original {
+			if elemFromOriginal == elemFromModified {
+				continue added
+			}
+		}
+		addedElems = append(addedElems, elemFromModified)
+	}
+	return deletedElems, addedElems
+}

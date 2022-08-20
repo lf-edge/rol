798e5fe1aaf2b9b32df32e87cf652185109ad6f9
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-20 23:43:31 +0300

    src: webapi: controllers: add host network vlan controller
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/webapi/controllers/GinHelpers.go
+++ src/webapi/controllers/GinHelpers.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"net/http"
+	"rol/app/errors"
+	"rol/dtos"
 )
 
 //RestoreBody restore body in gin.Context for logging it later in middleware
@@ -15,4 +18,79 @@ func RestoreBody(body interface{}, ctx *gin.Context) error {
 	}
 	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 	return nil
+}
+
+//getRequestDtoAndRestoreBody parse json body to dto object and restore body in context
+//for logging it later in middleware
+func getRequestDtoAndRestoreBody[reqDtoType any](ctx *gin.Context) (reqDtoType, error) {
+	reqDto := new(reqDtoType)
+	err := ctx.ShouldBindJSON(reqDto)
+	if err != nil {
+		return *reqDto, errors.Validation.New("incorrect json")
+	}
+	buf, err := json.Marshal(reqDto)
+	if err != nil {
+		return *reqDto, errors.Internal.New("failed to marshal object back tp json for logging")
+	}
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
+	return *reqDto, nil
+}
+
+func validationErrorToValidationErrorDto(err error) dtos.ValidationErrorDto {
+	validationErrorDto := dtos.ValidationErrorDto{
+		Message: err.Error(),
+		Errors:  []dtos.ValidationErrorElemDto{},
+	}
+	//For now, we can wrap validation error in the top level to another typed error
+	//and current error will be not validation error
+	//We need fix this later
+	if errors.GetType(err) == errors.Validation {
+		context := errors.GetErrorContext(err)
+		for key, value := range context {
+			validationErrorDto.Errors = append(
+				validationErrorDto.Errors,
+				dtos.ValidationErrorElemDto{
+					Source: "body",
+					Field:  key,
+					Error:  value,
+				})
+		}
+	}
+	return validationErrorDto
+}
+
+//abortWithStatusByErrorType call ctx.AbortWithStatus by error type,
+//if error = nil, then abort with http.StatusInternalServerError
+func abortWithStatusByErrorType(ctx *gin.Context, err error) {
+	if errors.As(err, errors.Internal) || errors.As(err, errors.NoType) {
+		ctx.AbortWithStatus(http.StatusInternalServerError)
+		return
+	} else if errors.As(err, errors.Validation) {
+		ctx.JSON(http.StatusBadRequest, validationErrorToValidationErrorDto(err))
+		return
+	} else if errors.As(err, errors.NotFound) {
+		ctx.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	ctx.AbortWithStatus(http.StatusInternalServerError)
+}
+
+//handle current request by error type, if error = nil, set http status to 204 (No Content)
+func handle(ctx *gin.Context, err error) {
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+	ctx.Status(http.StatusNoContent)
+}
+
+//handleWithData same as handle, but
+//if error = nil, set response body to data as json
+//and http status code to 200
+func handleWithData(ctx *gin.Context, err error, data interface{}) {
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, data)
 }
---
+++ src/webapi/controllers/HostNetworkVlanController.go
@@ -0,0 +1,154 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"rol/app/services"
+	"rol/dtos"
+	"rol/webapi"
+)
+
+//HostNetworkVlanController host network vlan API controller
+type HostNetworkVlanController struct {
+	service *services.HostNetworkVlanService
+	logger  *logrus.Logger
+}
+
+//NewHostNetworkVlanController host network vlan controller constructor. Parameters pass through DI
+//
+//Params:
+//	vlanService - vlan service
+//	log - logrus logger
+//Return:
+//	*HostNetworkVlanController - instance of host network vlan controller
+func NewHostNetworkVlanController(vlanService *services.HostNetworkVlanService, log *logrus.Logger) *HostNetworkVlanController {
+	return &HostNetworkVlanController{
+		service: vlanService,
+		logger:  log,
+	}
+}
+
+//RegisterHostNetworkVlanController registers controller for getting host network vlans via api
+func RegisterHostNetworkVlanController(controller *HostNetworkVlanController, server *webapi.GinHTTPServer) {
+	groupRoute := server.Engine.Group("/api/v1")
+
+	groupRoute.GET("/host/network/vlan/", controller.GetList)
+	groupRoute.GET("/host/network/vlan/:name", controller.GetByName)
+	groupRoute.POST("/host/network/vlan/", controller.Create)
+	groupRoute.PUT("/host/network/vlan/:name", controller.Update)
+	groupRoute.DELETE("/host/network/vlan/:name", controller.Delete)
+}
+
+//GetList get list of host network VLAN's
+//
+//Params:
+//	ctx - gin context
+//
+// @Summary Get list of host network VLAN's
+// @version	1.0
+// @Tags	host
+// @Accept	json
+// @Produce	json
+// @Success	200		{object}	[]dtos.HostNetworkVlanDto
+// @Failure	500		"Internal Server Error"
+// @router	/host/network/vlan/	[get]
+func (h *HostNetworkVlanController) GetList(ctx *gin.Context) {
+	vlanList, err := h.service.GetList()
+	handleWithData(ctx, err, vlanList)
+}
+
+//GetByName get vlan port by name
+//
+//Params:
+//	ctx - gin context
+//
+// @Summary	Gets vlan port by name
+// @version	1.0
+// @Tags	host
+// @Accept	json
+// @Produce	json
+// @param	name	path		string	true	"Vlan name"
+// @Success	200		{object}	dtos.HostNetworkVlanDto
+// @Failure	404		"Not Found"
+// @Failure	500		"Internal Server Error"
+// @router	/host/network/vlan/{name}	[get]
+func (h *HostNetworkVlanController) GetByName(ctx *gin.Context) {
+	name := ctx.Param("name")
+	vlan, err := h.service.GetByName(name)
+	handleWithData(ctx, err, vlan)
+}
+
+//Create new host vlan
+//
+//Params:
+//	ctx - gin context
+//
+// @Summary	Create new host vlan
+// @version	1.0
+// @Tags	host
+// @Accept	json
+// @Produce	json
+// @Param	request	body		dtos.HostNetworkVlanCreateDto	true	"Host vlan fields"
+// @Success	200		{object}	dtos.HostNetworkVlanDto
+// @Failure	400		{object}	dtos.ValidationErrorDto
+// @Failure	500		"Internal Server Error"
+// @router	/host/network/vlan/	[post]
+func (h *HostNetworkVlanController) Create(ctx *gin.Context) {
+	reqDto, err := getRequestDtoAndRestoreBody[dtos.HostNetworkVlanCreateDto](ctx)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+
+	vlanDto, err := h.service.Create(reqDto)
+	handleWithData(ctx, err, vlanDto)
+}
+
+//Update host network vlan
+//
+//Params:
+//	ctx - gin context
+//
+// @Summary update host network vlan
+// @version	1.0
+// @Tags	host
+// @Accept	json
+// @Produce	json
+// @Param	request	body		dtos.HostNetworkVlanUpdateDto	true	"Host vlan fields"
+// @param	name	path		string							false	"Vlan name"
+// @Success	200		{object}	dtos.HostNetworkVlanDto
+// @Failure	400		{object}	dtos.ValidationErrorDto
+// @Failure	404		"Not Found"
+// @Failure	500		"Internal Server Error"
+// @router	/host/network/vlan/{name}	[put]
+func (h *HostNetworkVlanController) Update(ctx *gin.Context) {
+	reqDto, err := getRequestDtoAndRestoreBody[dtos.HostNetworkVlanUpdateDto](ctx)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+	name := ctx.Param("name")
+	vlanDto, err := h.service.Update(name, reqDto)
+	handleWithData(ctx, err, vlanDto)
+}
+
+//Delete host network vlan
+//
+//Params:
+//	ctx - gin context
+//
+// @Summary	Delete host network vlan by name
+// @version	1.0
+// @Tags	host
+// @Accept	json
+// @Produce	json
+// @param	name	path		string	true	"Vlan name"
+// @Success	204		"OK, but No Content"
+// @Failure	404		"Not Found"
+// @Failure	500		"Internal Server Error"
+// @router	/host/network/vlan/{name}	[delete]
+func (h *HostNetworkVlanController) Delete(ctx *gin.Context) {
+	name := ctx.Param("name")
+	err := h.service.Delete(name)
+	handle(ctx, err)
+}

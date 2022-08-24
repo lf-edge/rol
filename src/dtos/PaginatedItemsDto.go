d4b70faa73ca52771ae9f6e3ae70ae683dd86361
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-24 18:01:31 +0300

    src: dtos: add constructors for PaginatedItemsDto
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/dtos/PaginatedItemsDto.go
+++ src/dtos/PaginatedItemsDto.go
@@ -13,9 +13,47 @@ type PaginationInfoDto struct {
 }
 
 //PaginatedItemsDto struct with slice of items and pagination info
-type PaginatedItemsDto[DataType any] struct {
+type PaginatedItemsDto[T any] struct {
 	//Pagination info about pagination
 	Pagination PaginationInfoDto
 	//Items slice of items
-	Items []DataType
+	Items []T
+}
+
+func pageCount(totalItems, pageSize int) int {
+	pages := totalItems / pageSize
+
+	if totalItems%pageSize > 0 {
+		pages++
+	}
+	if pages < 1 {
+		return 1
+	}
+	return pages
+}
+
+//NewEmptyPaginatedItemsDto constructor for PaginatedItemsDto[DataType] without data, used for errors
+func NewEmptyPaginatedItemsDto[T any]() PaginatedItemsDto[T] {
+	return PaginatedItemsDto[T]{
+		Pagination: PaginationInfoDto{
+			Page:       1,
+			Size:       10,
+			TotalCount: 0,
+			TotalPages: 1,
+		},
+		Items: []T{},
+	}
+}
+
+//NewPaginatedItemsDto constructor for PaginatedItemsDto[DataType]
+func NewPaginatedItemsDto[T any](page, size, totalCount int, items []T) PaginatedItemsDto[T] {
+	return PaginatedItemsDto[T]{
+		Pagination: PaginationInfoDto{
+			Page:       page,
+			Size:       size,
+			TotalCount: totalCount,
+			TotalPages: pageCount(totalCount, size),
+		},
+		Items: items,
+	}
 }

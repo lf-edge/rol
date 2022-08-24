c6275328ebe6544d4affdd7800c090c27b3a9960
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-24 17:59:34 +0300

    src: app: validators: switch to universal error wrapper function for ozzo validation errors
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/app/validators/EthernetSwitchPortUpdateDto.go
+++ src/app/validators/EthernetSwitchPortUpdateDto.go
@@ -2,7 +2,6 @@ package validators
 
 import (
 	validation "github.com/go-ozzo/ozzo-validation"
-	"rol/app/errors"
 	"rol/dtos"
 )
 
@@ -10,8 +9,7 @@ import (
 //	Return
 //	error - if an error occurs, otherwise nil
 func ValidateEthernetSwitchPortUpdateDto(dto dtos.EthernetSwitchPortUpdateDto) error {
-	var err error
-	validationErr := validation.ValidateStruct(&dto,
+	err := validation.ValidateStruct(&dto,
 		validation.Field(&dto.Name, []validation.Rule{
 			validation.Required,
 			validation.By(trimValidation),
@@ -23,11 +21,5 @@ func ValidateEthernetSwitchPortUpdateDto(dto dtos.EthernetSwitchPortUpdateDto) e
 			validation.By(containsSpacesValidation),
 			validation.By(validatePOEType),
 		}...))
-	if validationErr != nil {
-		err = errors.Validation.New(errors.ValidationErrorMessage)
-		for key, value := range validationErr.(validation.Errors) {
-			err = errors.AddErrorContext(err, key, value.Error())
-		}
-	}
-	return err
+	return convertOzzoErrorToValidationError(err)
 }
--- src/app/validators/EthernetSwitchUpdateDto.go
+++ src/app/validators/EthernetSwitchUpdateDto.go
@@ -3,7 +3,6 @@ package validators
 import (
 	validation "github.com/go-ozzo/ozzo-validation"
 	"regexp"
-	"rol/app/errors"
 	"rol/dtos"
 )
 
@@ -11,8 +10,7 @@ import (
 //	Return
 //	error - if an error occurs, otherwise nil
 func ValidateEthernetSwitchUpdateDto(dto dtos.EthernetSwitchUpdateDto) error {
-	var err error
-	validationErr := validation.ValidateStruct(&dto,
+	err := validation.ValidateStruct(&dto,
 		validation.Field(&dto.Name, []validation.Rule{
 			validation.Required,
 			validation.By(trimValidation),
@@ -41,11 +39,5 @@ func ValidateEthernetSwitchUpdateDto(dto dtos.EthernetSwitchUpdateDto) error {
 			validation.Required,
 			validation.By(trimValidation),
 		}...))
-	if validationErr != nil {
-		err = errors.Validation.New(errors.ValidationErrorMessage)
-		for key, value := range validationErr.(validation.Errors) {
-			err = errors.AddErrorContext(err, key, value.Error())
-		}
-	}
-	return err
+	return convertOzzoErrorToValidationError(err)
 }

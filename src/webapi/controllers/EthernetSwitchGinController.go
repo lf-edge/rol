3b5d72e33a9d4a95bbddc0fda828fd8cfcbba747
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-25 21:27:33 +0300

    src: webapi: controllers: remove depends to GinGenericController from ethernet switch controller
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/webapi/controllers/EthernetSwitchGinController.go
+++ src/webapi/controllers/EthernetSwitchGinController.go
@@ -2,21 +2,20 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 	"rol/app/services"
-	"rol/domain"
 	"rol/dtos"
 	"rol/webapi"
+	"strconv"
 
 	"github.com/sirupsen/logrus"
 )
 
 //EthernetSwitchGinController ethernet switch GIN controller constructor
 type EthernetSwitchGinController struct {
-	GinGenericController[dtos.EthernetSwitchDto,
-		dtos.EthernetSwitchCreateDto,
-		dtos.EthernetSwitchUpdateDto,
-		domain.EthernetSwitch]
+	service *services.EthernetSwitchService
+	logger  *logrus.Logger
 }
 
 //RegisterEthernetSwitchController registers controller for the switches on path /api/v1/ethernet-switch/
@@ -28,6 +27,20 @@ func RegisterEthernetSwitchController(controller *EthernetSwitchGinController, s
 	groupRoute.POST("/ethernet-switch", controller.Create)
 	groupRoute.PUT("/ethernet-switch/:id", controller.Update)
 	groupRoute.DELETE("/ethernet-switch/:id", controller.Delete)
+}
+
+//NewEthernetSwitchGinController ethernet switch controller constructor. Parameters pass through DI
+//Params
+//	service - generic service
+//	log - logrus logger
+//Return
+//	*GinGenericController - instance of generic controller for ethernet switches
+func NewEthernetSwitchGinController(service *services.EthernetSwitchService, log *logrus.Logger) *EthernetSwitchGinController {
+	switchContr := &EthernetSwitchGinController{
+		service: service,
+		logger:  log,
+	}
+	return switchContr
 }
 
 //GetList get list of switches with search and pagination
@@ -47,7 +60,21 @@ func RegisterEthernetSwitchController(controller *EthernetSwitchGinController, s
 // @Failure	500		"Internal Server Error"
 // @router /ethernet-switch/ [get]
 func (e *EthernetSwitchGinController) GetList(ctx *gin.Context) {
-	e.GinGenericController.GetList(ctx)
+	orderBy := ctx.DefaultQuery("orderBy", "id")
+	orderDirection := ctx.DefaultQuery("orderDirection", "asc")
+	search := ctx.DefaultQuery("search", "")
+	page := ctx.DefaultQuery("page", "1")
+	pageInt64, err := strconv.ParseInt(page, 10, 64)
+	if err != nil {
+		pageInt64 = 1
+	}
+	pageSize := ctx.DefaultQuery("pageSize", "10")
+	pageSizeInt64, err := strconv.ParseInt(pageSize, 10, 64)
+	if err != nil {
+		pageSizeInt64 = 10
+	}
+	paginatedList, err := e.service.GetList(ctx, search, orderBy, orderDirection, int(pageInt64), int(pageSizeInt64))
+	handleWithData(ctx, err, paginatedList)
 }
 
 //GetByID get switch by id
@@ -64,7 +91,14 @@ func (e *EthernetSwitchGinController) GetList(ctx *gin.Context) {
 // @Failure	500		"Internal Server Error"
 // @router /ethernet-switch/{id} [get]
 func (e *EthernetSwitchGinController) GetByID(ctx *gin.Context) {
-	e.GinGenericController.GetByID(ctx)
+	strID := ctx.Param("id")
+	id, err := uuid.Parse(strID)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+	dto, err := e.service.GetByID(ctx, id)
+	handleWithData(ctx, err, dto)
 }
 
 //Create new switch
@@ -81,7 +115,14 @@ func (e *EthernetSwitchGinController) GetByID(ctx *gin.Context) {
 // @Failure	500		"Internal Server Error"
 // @router /ethernet-switch/ [post]
 func (e *EthernetSwitchGinController) Create(ctx *gin.Context) {
-	e.GinGenericController.Create(ctx)
+	reqDto, err := getRequestDtoAndRestoreBody[dtos.EthernetSwitchCreateDto](ctx)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+
+	dto, err := e.service.Create(ctx, reqDto)
+	handleWithData(ctx, err, dto)
 }
 
 //Update switch by id
@@ -100,7 +141,19 @@ func (e *EthernetSwitchGinController) Create(ctx *gin.Context) {
 // @Failure	500		"Internal Server Error"
 // @router /ethernet-switch/{id} [put]
 func (e *EthernetSwitchGinController) Update(ctx *gin.Context) {
-	e.GinGenericController.Update(ctx)
+	reqDto, err := getRequestDtoAndRestoreBody[dtos.EthernetSwitchUpdateDto](ctx)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+	id, err := parseUUIDParam(ctx, "id")
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+
+	dto, err := e.service.Update(ctx, reqDto, id)
+	handleWithData(ctx, err, dto)
 }
 
 //Delete soft deleting switch in database
@@ -117,7 +170,15 @@ func (e *EthernetSwitchGinController) Update(ctx *gin.Context) {
 // @Failure	500		"Internal Server Error"
 // @router /ethernet-switch/{id} [delete]
 func (e *EthernetSwitchGinController) Delete(ctx *gin.Context) {
-	e.GinGenericController.Delete(ctx)
+	strID := ctx.Param("id")
+	id, err := uuid.Parse(strID)
+	if err != nil {
+		abortWithStatusByErrorType(ctx, err)
+		return
+	}
+
+	err = e.service.Delete(ctx, id)
+	handle(ctx, err)
 }
 
 //GetSupportedModels Get supported switch models
@@ -131,24 +192,6 @@ func (e *EthernetSwitchGinController) Delete(ctx *gin.Context) {
 // @Success	200		{object} []dtos.EthernetSwitchModelDto
 // @router /ethernet-switch/models [get]
 func (e *EthernetSwitchGinController) GetSupportedModels(ctx *gin.Context) {
-	service := e.GinGenericController.service.(*services.EthernetSwitchService)
-	modelsDtoSlice := service.GetSupportedModels()
+	modelsDtoSlice := e.service.GetSupportedModels()
 	ctx.JSON(http.StatusOK, modelsDtoSlice)
-}
-
-//NewEthernetSwitchGinController ethernet switch controller constructor. Parameters pass through DI
-//Params
-//	service - generic service
-//	log - logrus logger
-//Return
-//	*GinGenericController - instance of generic controller for ethernet switches
-func NewEthernetSwitchGinController(service *services.EthernetSwitchService, log *logrus.Logger) *EthernetSwitchGinController {
-	genContr := NewGinGenericController[dtos.EthernetSwitchDto,
-		dtos.EthernetSwitchCreateDto,
-		dtos.EthernetSwitchUpdateDto,
-		domain.EthernetSwitch](service, log)
-	switchContr := &EthernetSwitchGinController{
-		GinGenericController: *genContr,
-	}
-	return switchContr
 }

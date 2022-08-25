623b500a7e53d9ff4c07793e2604330f56f87d94
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-25 21:22:02 +0300

    src: app: services: http log service was unbound from IGenericService and GenericService struct
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/app/services/HttpLogService.go
+++ src/app/services/HttpLogService.go
@@ -1,7 +1,8 @@
 package services
 
 import (
-	"rol/app/errors"
+	"context"
+	"github.com/google/uuid"
 	"rol/app/interfaces"
 	"rol/domain"
 	"rol/dtos"
@@ -11,29 +12,47 @@ import (
 
 //HTTPLogService service structure for HTTPLog entity
 type HTTPLogService struct {
-	*GenericService[
-		dtos.HTTPLogDto,
-		dtos.HTTPLogDto,
-		dtos.HTTPLogDto,
-		domain.HTTPLog]
+	repository interfaces.IGenericRepository[domain.HTTPLog]
+	logger     *logrus.Logger
 }
 
 //NewHTTPLogService preparing domain.HTTPLog repository for passing it in DI
 //Params
-//	rep - generic repository with http log instantiated
+//	repo - generic repository with HTTP log instantiated
 //	log - logrus logger
 //Return
 //	New http log service
-func NewHTTPLogService(rep interfaces.IGenericRepository[domain.HTTPLog], log *logrus.Logger) (interfaces.IGenericService[
-	dtos.HTTPLogDto,
-	dtos.HTTPLogDto,
-	dtos.HTTPLogDto,
-	domain.HTTPLog], error) {
-	genericSerice, err := NewGenericService[dtos.HTTPLogDto, dtos.HTTPLogDto, dtos.HTTPLogDto](rep, log)
-	if err != nil {
-		return nil, errors.Internal.Wrap(err, "error constructing http log service")
-	}
-	return HTTPLogService{
-		genericSerice,
+func NewHTTPLogService(repo interfaces.IGenericRepository[domain.HTTPLog], log *logrus.Logger) (*HTTPLogService, error) {
+	return &HTTPLogService{
+		repository: repo,
+		logger:     log,
 	}, nil
+}
+
+//GetList Get list of HTTP logs dto elements with search in all fields and pagination
+//
+//Params:
+//	ctx - context is used only for logging
+//	search - string for search in entity string fields
+//	orderBy - order by entity field name
+//	orderDirection - ascending or descending order
+//	page - page number
+//	pageSize - page size
+//Return:
+//	dtos.PaginatedItemsDto[dtos.HTTPLogDto] - paginated items
+//	error - if an error occurs, otherwise nil
+func (h *HTTPLogService) GetList(ctx context.Context, search string, orderBy string, orderDirection string, page int, pageSize int) (dtos.PaginatedItemsDto[dtos.HTTPLogDto], error) {
+	return GetList[dtos.HTTPLogDto](ctx, h.repository, search, orderBy, orderDirection, page, pageSize)
+}
+
+//GetByID Get HTTP log dto by ID
+//
+//Params:
+//	ctx - context
+//	id - entity id
+//Return:
+//	dtos.HTTPLogDto - http log dto
+//	error - if an error occurs, otherwise nil
+func (h *HTTPLogService) GetByID(ctx context.Context, id uuid.UUID) (dtos.HTTPLogDto, error) {
+	return GetByID[dtos.HTTPLogDto](ctx, h.repository, id, nil)
 }

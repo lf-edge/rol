d6204c8a7cf81017aa5630d14991825189a32e50
Dmitrii Aleksandrov <goodmobiledevices@gmail.com>
2022-08-25 21:12:03 +0300

    src: app: services: AppLogService was unbound from IGenericService and GenericService implementation
    Signed-off-by: Dmitrii Aleksandrov <goodmobiledevices@gmail.com>

--- src/app/services/AppLogService.go
+++ src/app/services/AppLogService.go
@@ -1,38 +1,59 @@
 package services
 
 import (
-	"rol/app/errors"
+	"context"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 	"rol/app/interfaces"
 	"rol/domain"
 	"rol/dtos"
-
-	"github.com/sirupsen/logrus"
 )
 
 //AppLogService service structure for AppLog entity
 type AppLogService struct {
-	*GenericService[dtos.AppLogDto,
-		dtos.AppLogDto,
-		dtos.AppLogDto,
-		domain.AppLog]
+	repository interfaces.IGenericRepository[domain.AppLog]
+	logger     *logrus.Logger
 }
 
 //NewAppLogService preparing domain.AppLog repository for passing it in DI
-//Params
-//	rep - generic repository with http log instantiated
+//
+//Params:
+//	repo - generic repository with app log instantiated
 //	log - logrus logger
-//Return
-//	New app log service
-func NewAppLogService(rep interfaces.IGenericRepository[domain.AppLog], log *logrus.Logger) (interfaces.IGenericService[
-	dtos.AppLogDto,
-	dtos.AppLogDto,
-	dtos.AppLogDto,
-	domain.AppLog], error) {
-	genericService, err := NewGenericService[dtos.AppLogDto, dtos.AppLogDto, dtos.AppLogDto](rep, log)
-	if err != nil {
-		return nil, errors.Internal.Wrap(err, "error constructing app log service")
-	}
+//Return:
+//	*AppLogService - new app log service
+//	error - if an error occurs, otherwise nil
+func NewAppLogService(repo interfaces.IGenericRepository[domain.AppLog], log *logrus.Logger) (*AppLogService, error) {
 	return &AppLogService{
-		genericService,
+		repository: repo,
+		logger:     log,
 	}, nil
+}
+
+//GetList Get list of logs dto elements with search in all fields and pagination
+//
+//Params:
+//	ctx - context is used only for logging
+//	search - string for search in entity string fields
+//	orderBy - order by entity field name
+//	orderDirection - ascending or descending order
+//	page - page number
+//	pageSize - page size
+//Return:
+//	dtos.PaginatedItemsDto[dtos.AppLogDto] - paginated items
+//	error - if an error occurs, otherwise nil
+func (a *AppLogService) GetList(ctx context.Context, search string, orderBy string, orderDirection string, page int, pageSize int) (dtos.PaginatedItemsDto[dtos.AppLogDto], error) {
+	return GetList[dtos.AppLogDto](ctx, a.repository, search, orderBy, orderDirection, page, pageSize)
+}
+
+//GetByID Get log dto by ID
+//
+//Params:
+//	ctx - context
+//	id - entity id
+//Return:
+//	dtos.AppLogDto - app log dto
+//	error - if an error occurs, otherwise nil
+func (a *AppLogService) GetByID(ctx context.Context, id uuid.UUID) (dtos.AppLogDto, error) {
+	return GetByID[dtos.AppLogDto](ctx, a.repository, id, nil)
 }
